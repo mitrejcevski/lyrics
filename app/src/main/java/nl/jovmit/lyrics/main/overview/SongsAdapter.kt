1689cbdb00e63739693cbf82e2f379fd57502624
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2019-07-15 10:57:00 +0200

    Temp

---
+++ app/src/main/java/nl/jovmit/lyrics/main/overview/SongsAdapter.kt
@@ -0,0 +1,36 @@
+package nl.jovmit.lyrics.main.overview
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import nl.jovmit.lyrics.R
+import nl.jovmit.lyrics.extensions.inflate
+import nl.jovmit.lyrics.main.data.Song
+
+class SongsAdapter : RecyclerView.Adapter<SongsAdapter.ViewHolder>() {
+
+    private val songs = mutableListOf<Song>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.song_list_item)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(songs[position])
+    }
+
+    override fun getItemCount(): Int = songs.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val title: TextView = itemView.findViewById(R.id.songListItemTitle)
+        private val singer: TextView = itemView.findViewById(R.id.songListItemSinger)
+
+        fun bind(song: Song) {
+            title.text = song.title
+            singer.text = song.singer
+        }
+    }
+}
