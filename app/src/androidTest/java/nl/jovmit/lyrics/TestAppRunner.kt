22731a3db6d548f1e8569e616f94e51a4368944b
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2019-08-06 12:31:06 +0200

    Custom application for testing and custom runner

---
+++ app/src/androidTest/java/nl/jovmit/lyrics/TestAppRunner.kt
@@ -0,0 +1,17 @@
+package nl.jovmit.lyrics
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import nl.jovmit.lyrics.app.TestApp
+
+@SuppressWarnings("unused")
+class TestAppRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(
+        loader: ClassLoader?,
+        className: String?,
+        context: Context?): Application {
+        return super.newApplication(loader, TestApp::class.java.name, context)
+    }
+}
--- app/src/main/java/nl/jovmit/lyrics/main/mainModule.kt
+++ app/src/main/java/nl/jovmit/lyrics/main/mainModule.kt
@@ -1,7 +1,14 @@
 package nl.jovmit.lyrics.main
 
+import nl.jovmit.lyrics.common.AppCoroutineDispatchers
+import nl.jovmit.lyrics.common.CoroutineDispatchers
+import nl.jovmit.lyrics.main.overview.FirebaseSongsService
+import nl.jovmit.lyrics.main.overview.SongsOverviewViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mainModule = module {
-
+    single<CoroutineDispatchers> { AppCoroutineDispatchers() }
+    factory { SongsRepository(FirebaseSongsService()) }
+    viewModel { SongsOverviewViewModel(get(), get()) }
 }
