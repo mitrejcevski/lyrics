e9b9e540c619fe3d0cfee5f3b152045aad6dca31
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-27 08:25:06 +0100

    Remove unused testing module

--- app/src/androidTest/java/nl/jovmit/lyrics/main/testModule.kt
+++ app/src/androidTest/java/nl/jovmit/lyrics/main/testModule.kt
@@ -7,21 +7,9 @@ import nl.jovmit.lyrics.main.add.NewSongViewModel
 import nl.jovmit.lyrics.main.details.SongDetailsViewModel
 import nl.jovmit.lyrics.main.overview.SongsRepository
 import nl.jovmit.lyrics.main.overview.SongsViewModel
-import nl.jovmit.lyrics.utils.IdGenerator
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
-
-val inMemoryTestModule = module {
-    single<CoroutineDispatchers> { AppCoroutineDispatchers() }
-    single { IdGenerator() }
-    single<SongsService> { InMemorySongsService(get()) }
-    factory { NewSongRepository(get()) }
-    factory { SongsRepository(get()) }
-    viewModel { NewSongViewModel(get(), get()) }
-    viewModel { InfoViewModel() }
-    viewModel { SongsViewModel(get(), get()) }
-}
 
 fun testModuleWithCustomSongsService(songsService: SongsService): Module {
     return module {
